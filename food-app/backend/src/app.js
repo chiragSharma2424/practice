938394da79d04a112281384184617c5507ef21d0
chirag <sharmachirag242004@gmail.com>
2025-11-10 16:47:24 +0530

    user created and token saved in token

--- food-app/backend/src/app.js
+++ food-app/backend/src/app.js
@@ -2,11 +2,15 @@
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const authRoutes = require('./routes/auth.routes');
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
+
+// routes
+app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => {
     res.send("hello world");
